63ec17454eeb8371dfdeba8e647b1f169a60515b
JuanHenao1999 <juanhenao123kl@gmail.com>
2023-08-30 07:54:09 -0500

    Wednesday class

---
+++ demo UPB/data.js
@@ -0,0 +1,11 @@
+// const alumnos = ["Jose", "Jorge", "Juan"];
+
+// module.exports = alumnos
+
+ export const alumnos = ["Jose", "Jorge", "Juan"];
+
+ export const message = () => {
+    console.log("Obteniendo alumnos");
+ }
+ 
+// export default alumnos;
---
+++ demo UPB/index.js
@@ -0,0 +1,10 @@
+// const dataAlumnos = require("./data")
+// console.log(dataAlumnos);
+
+// import alumnos from "./data.js";
+// console.log(alumnos);
+
+import { alumnos } from "./data.js";
+console.log(alumnos);
+import { message } from "./data.js";
+message()
